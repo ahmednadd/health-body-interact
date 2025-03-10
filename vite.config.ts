95fdf78488027ce4af93d1ef2326b91de90e4af2
nadd <ahmed.nadd@outlook.com>
2025-03-10 13:33:11 +0530

    updated routing logic

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/health-body-interact/',
+  base:  mode === 'production' ? '/health-body-interact/' : '/',
   server: {
     host: "::",
     port: 8080,
